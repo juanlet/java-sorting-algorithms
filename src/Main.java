be3808459e65a7de079f7b8e64ef93809b42eba5
JAF <juanf03@gmail.com>
2017-01-24 23:14:59 -0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	
+
+		
+	public static void main (String [ ] args) {
+		 
+		 int[] array= {6,5,4,3,2,1};
+		 
+
+		 SortManager sortManager=new SortManager();
+		 
+		 
+		 
+		 //array=sortManager.selectionSort(array);
+		 //array=sortManager.bubbleSort(array);
+		 array=sortManager.insertionSort(array);
+	
+}
+
+}
---
+++ src/SortManager.java
@@ -0,0 +1,101 @@
+public class SortManager {
+
+      private void swap(int[] array,int indexA, int indexB){
+
+    	  int temp=array[indexA];
+    	  array[indexA]=array[indexB];
+    	  array[indexB]=temp;
+
+
+      }
+
+      private void print(int[] array){
+
+		 for(int i=0;i<array.length;i++){
+
+			 System.out.print(array[i]+",");
+
+		 }
+
+		 System.out.println("");
+
+      }
+
+	   public int[] selectionSort(int[] array){
+
+		   for(int i=0;i<array.length;i++){
+
+			   for(int j=i+1;j<array.length;j++){
+
+				   if(array[i]>array[j]){
+					   swap(array, i, j);
+					   print(array);
+				   }
+
+			   }
+
+		   }
+
+		   return array;
+	   }
+
+	   public int[] bubbleSort(int[] array){
+
+
+
+		  int times=0;
+
+		   for(int i=0,len=array.length;i<len;i++){
+			    boolean sorted=true;
+
+			   for(int j=array.length-1;j>i;j--){
+
+				   if(array[j]<array[j-1]){
+					   swap(array, j, j-1);
+					   print(array);
+					   sorted=false;
+
+				   }
+				   times++;
+
+			   }
+
+
+
+			   if(sorted){
+				   break;
+			   }
+
+		   }
+		   System.out.println(times);
+		   return array;
+
+
+	   }
+
+	 public int[] insertionSort(int[] array){
+
+         print(array);
+       
+	       for(int i=1;i<array.length;i++){
+	    	   	//bubble sort to position the element on the right spot of the sorted sublist
+			     for(int j=i;j>0;j--){
+			    	 
+			    	 if(array[j]<array[j-1]){
+			    		 swap(array,j-1,j);
+			  	         print(array);
+			    	 }else{
+			    		//si no fue menor a j-1 no va a ser menor a j-2 porque ese subset ya está ordenado 
+			    		break; 
+			    	 }
+									
+				 }
+	
+			}
+				 
+			 return array;
+
+	  }
+
+
+}
