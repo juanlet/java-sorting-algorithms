fa77272a02899b285ef54db19526958a85408785
JAF <juanf03@gmail.com>
2017-01-25 00:56:25 -0300

    Added binary search

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,18 @@ public class Main {
 	public static void main (String [ ] args) {
 		 
 		 int[] array= {6,5,4,3,2,1};
-		 
+		 int[] array2={1,2,3,4,5,6};
 
 		 SortManager sortManager=new SortManager();
-		 
+		 SearchManager searchManager=new SearchManager();
 		 
 		 
 		 //array=sortManager.selectionSort(array);
 		 //array=sortManager.bubbleSort(array);
-		 array=sortManager.insertionSort(array);
+		 //array=sortManager.insertionSort(array);
 	
+		 searchManager.binarySearch(array2, 4);
+		 
 }
 
 }
